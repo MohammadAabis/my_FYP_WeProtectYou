5b01d5cb30a524ce6c505b81ac21e562c79ba7d4
MohammadAabis <abiss.ali512@gmail.com>
2022-07-18 13:00:34 +0500

    Insert Query done

--- Controllers/NeedyOperations/UploadPostController.cs
+++ Controllers/NeedyOperations/UploadPostController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProtectYou.Models;
+using System.Data.SqlClient;
 
 namespace ProtectYou.Controllers.NeedyOperations
 {
     public class UploadPostController : Controller
     {
+        SqlConnection dbConn = new SqlConnection(@"Data Source=DESKTOP-7KVCKFN\SQLEXPRESS;Initial Catalog=ProtectYou;Integrated Security=True;MultipleActiveResultSets=True");
+        SqlDataAdapter dAdapter = new SqlDataAdapter();
         public IActionResult UploadPost()
         {
             return View(); 
@@ -17,8 +20,32 @@ namespace ProtectYou.Controllers.NeedyOperations
         [HttpPost]
         public IActionResult UploadPost(PostRequirements post)
         {
-            
-            return View();
+            string status = "Success";
+            string requirements = HttpContext.Request.Form["requirement"];
+            double money = Convert.ToDouble(HttpContext.Request.Form["money"]);
+            string description = HttpContext.Request.Form["description"];
+
+            if(requirements != "" && money > 0 && description != "")
+            {
+                status = "";
+                dbConn.Open();
+                string qry = "INSERT INTO postRequest (requirement,quantity,description) Values('" + requirements + "', '" + money + "', '" + description + "')";
+                SqlCommand insertCmd = new SqlCommand(qry, dbConn);
+                dAdapter.InsertCommand = insertCmd;
+                dAdapter.InsertCommand.ExecuteNonQuery();
+
+                dbConn.Close();
+
+
+
+                status = "Data inserted Successfully.";
+            }
+            else
+            {
+                status = "Form fields should not be empty.";
+            }
+
+            return Content(status);
         }
     }
 }
